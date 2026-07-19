9da00324764b2d93ca209f13203bd6204c9e781c
kalavathimlops <kalavathimlops@gmail.com>
2026-07-19 15:13:23 -0500

    Add: python-list-comprehension - submission-0

---
+++ Python Coding Interviews/python-list-comprehension/submission-0.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def create_list_of_odds(n:int)->List[int]:
+    return [i for i in range(1,n+1,2)]
+
+print(create_list_of_odds(1))
+print(create_list_of_odds(5))
+print(create_list_of_odds(6))
+print(create_list_of_odds(10))    
