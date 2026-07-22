c40ee58c3460951f215f55098e62b5687f126757
kalavathimlops <kalavathimlops@gmail.com>
2026-07-22 16:26:17 -0500

    Add: python-intro-to-dictionaries - submission-0

---
+++ Python For Beginners/python-intro-to-dictionaries/submission-0.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def create_dict(name:str,age:int)->Dict[str,int]:
+    pass
+
+def list_to_dict(words:List[str]) -> Dict[str,int]:
+    pass
+
+print(create_dict("Alice", 25))
+print(create_dict("Jane", 35))
+print(create_dict("Joe", 45))
+
+print(list_to_dict(["Alice", "Jane", "Joe"]))
+print(list_to_dict(["Apple", "Banana", "Watermelon", "Pineapple"]))
